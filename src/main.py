8252cd77e883097282c28cd8ba779c153ceff211
Taka <amano.takahisa@gmail.com>
2022-08-10 20:56:54 +0200

    at least working but not actual ranking

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import logging
 from pprint import pprint
 from slack_bolt import App
 from slack_sdk.web import WebClient
-from typing import List, Dict, Optional, Any, Union
+from typing import List, Dict, Optional, Any, Union, NamedTuple
 from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger(__name__)
@@ -62,6 +62,72 @@ def get_number_of_messages_today(
                              if float(message['ts']) >= ts_24h_ago]
         return len(messages_last_24h)
 
+    except SlackApiError as e:
+        logger.error("Error creating conversation: {}".format(e))
+
+
+class MessageCount(NamedTuple):
+    channel_id: str
+    channel_name: str
+    channel_topic: str
+    message_count: int
+
+
+def compose_message_blocks(message_counts: List[MessageCount],
+                           ) -> List[Dict[str, Any]]:
+    n_channels = len(message_counts)
+    total_messages = sum([mc.message_count for mc in message_counts])
+    message_counts = sorted(message_counts,
+                            key=lambda item: getattr(item, 'message_count'),
+                            reverse=True)
+    stat_blocks = []
+    for idx, message_count in enumerate(message_counts):
+        if idx == 0:
+            icon = ':first_place_medal:'
+
+        else:
+            icon = ':tada:'
+        stat_block = f'{icon} {idx + 1}. #{message_count.channel_name}'
+
+        stat_blocks.append(stat_block)
+    # return stat_messages
+    blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": ":star2: Today's Active Channel Rankings :star2:"
+            }
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": ":first_place_medal: #general"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": "30 messagess today"
+                }
+            ]
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "text": "xxxxx channel_topic here xxxxx",
+                    "type": "mrkdwn"
+                }
+            ]
+        },
+        {
+            "type": "divider"
+        }
+    ]
+
+    return blocks
+
 
 def post_message(channel, client: WebClient, **kwargs):
     try:
@@ -73,8 +139,44 @@ def post_message(channel, client: WebClient, **kwargs):
 
 
 def main(*args, **kwargs):
+    client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+    # configs = get_slack_configs()
+    channel_list = get_channel_list(client=client)
+    hot_channel_id, is_member_hot_channel = [
+        (c['id'], c['is_member']) for c in channel_list
+        if c['name'] == CHANNEL_NAME][0]
 
-    pass
+    if is_member_hot_channel is False:
+        join_to_channel(channel_id=hot_channel_id, client=client)
+
+    # drop non_hot_channels
+    channel_list = [
+        c for c in channel_list if c['name'] not in EXCLUDE_FROM_STAT]
+
+    # join to channels
+    for channel in channel_list:
+        if not channel['is_member']:
+            join_to_channel(channel_id=channel['id'], client=client)
+            logger.info(f'The bot joined to channel "{channel["name"]}"')
+
+    message_counts: List[MessageCount] = []
+    for channel in channel_list:
+        n = get_number_of_messages_today(
+            channel_id=channel['id'], client=client)
+        message_counts.append(
+            MessageCount(channel_id=channel['id'],
+                         channel_name=channel['name'],
+                         channel_topic=channel['topick_value'],
+                         message_count=n))
+
+    blocks = compose_message_blocks(message_counts=message_counts)
+    pprint(blocks)
+    post_message(
+        channel=hot_channel_id,
+
+        client=client,
+        blocks=blocks,
+        text='Hot channel ranking')
 
 
 if __name__ == '__main__':
