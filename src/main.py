e0571245432b7d63b4ff1251f0cb5a288d1dc381
Taka <amano.takahisa@gmail.com>
2022-08-10 20:01:17 +0200

    update get_channel_list function

--- src/main.py
+++ src/main.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
 import os
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 from pprint import pprint
 from slack_bolt import App
 from slack_sdk.web import WebClient
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any, Union
 from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger(__name__)
 
-channel_name = 'hot-channels'
+CHANNEL_NAME = 'hot-channels'
 
-client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+# Channel names to be excluded from the ranking
+# NON_HOT_CHANNELS = ['hot-channels', 'notification']
+EXCLUDE_FROM_STAT = ['hot-channels']
 
 
 def get_slack_configs():
     pass
 
 
-def get_channel_list() -> List[Dict[str, str]]:
+def get_channel_list(
+        client: WebClient) -> List[Dict[str, str]]:  # type: ignore
     try:
         # Call the conversations.list method using the WebClient
         result = client.conversations_list()
         channel_list: List[Dict[str, str]] = [
-            {'id': c['id'], 'name': c['name'],
+            {'id': c['id'],
+             'name': c['name'],
+             'is_member': c['is_member'],
              'topick_value': c['topic']['value']}
             for c in result['channels'] if c['is_channel']]  # type: ignore
         return channel_list
