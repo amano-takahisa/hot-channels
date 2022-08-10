ca92bff7c8cff1fcfe4e30b01784b59f3a35052e
Taka <amano.takahisa@gmail.com>
2022-08-10 12:03:56 +0200

    add empty module

---
+++ src/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def get_slack_configs():
+    pass
+
+
+def get_messages():
+    pass
+
+
+def calc_stats():
+    pass
+
+
+def compose_message():
+    pass
+
+
+def post_message():
+    pass
+
+
+def main(*args, **kwargs):
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
